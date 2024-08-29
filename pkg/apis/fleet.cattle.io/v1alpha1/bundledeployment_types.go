25f73d04dce2729c81d5974669975b9dac956e0a
Patrick Seidensal <pseidensal@suse.com>
2024-08-29 15:40:37 +0200

    Update description of new field Exists

--- pkg/apis/fleet.cattle.io/v1alpha1/bundledeployment_types.go
+++ pkg/apis/fleet.cattle.io/v1alpha1/bundledeployment_types.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/rancher/wrangler/v3/pkg/genericcondition"
-	"github.com/rancher/wrangler/v3/pkg/summary"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/rancher/wrangler/v3/pkg/genericcondition"
+	"github.com/rancher/wrangler/v3/pkg/summary"
 )
 
 const BundleDeploymentResourceNamePlural = "bundledeployments"
@@ -407,8 +408,9 @@ type ModifiedStatus struct {
 	// +nullable
 	Name   string `json:"name,omitempty"`
 	Create bool   `json:"missing,omitempty"`
-	Exist  bool   `json:"exist,omitempty"`
-	Delete bool   `json:"delete,omitempty"`
+	// Exist is true if the resource exists but is not owned by us. This can happen if a resource was adopted by another bundle whereas the first bundle still exists and due to that reports that it does not own it.
+	Exist  bool `json:"exist,omitempty"`
+	Delete bool `json:"delete,omitempty"`
 	// +nullable
 	Patch string `json:"patch,omitempty"`
 }
